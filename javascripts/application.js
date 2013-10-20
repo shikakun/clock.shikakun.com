2759bcf2af3f9ab932a45aa66eb8bfe691857d10
shikakun <k@u-n.jp>
2013-10-20 22:13:09 +0900

    現在時刻を表示できるようにした

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,3 +1,20 @@
 $(function() {
-
+  getCurrentTime();
+  setInterval("getCurrentTime()", 500);
 });
+
+function getCurrentTime() {
+  var date = new Date();
+  var hour = makeNumSameLength(date.getHours(), 2);
+  var min = makeNumSameLength(date.getMinutes(), 2);
+  var sec = makeNumSameLength(date.getSeconds(), 2);
+  $('#time').html(hour + ':' + min + ':' + sec);
+}
+
+function makeNumSameLength (num, figures) {
+  var num = String(num);
+  while (num.length < figures) {
+    num = '0' + num;
+  }
+  return num;
+}
