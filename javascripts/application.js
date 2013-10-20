87cc09bafc9e4a6adac43f682ae4d709da1e5a94
shikakun <k@u-n.jp>
2013-10-20 23:39:18 +0900

    表示が切り替わったら15分間のカウントダウンがはじまるようにした

--- javascripts/application.js
+++ javascripts/application.js
@@ -2,7 +2,7 @@ $(function() {
   $('html').attr('data-mode', 'current');
   switchMode();
 
-  var current, remaining;
+  var current, remaining, date, hour, min, sec, minRaw, secRaw;
 
   $('#time').click(function () {
     switchMode();
@@ -12,40 +12,48 @@ $(function() {
 function switchMode() {
   var mode = $('html').attr('data-mode');
   if (mode == 'current') {
-    if ($('html').attr('data-status-remaining') > 0) {
-      clearInterval(remaining);
-      $('html').attr('data-status-remaining', 0);
-    }
     getCurrentTime();
     current = setInterval("getCurrentTime()", 500);
     mode = 'remaining';
   } else if (mode == 'remaining') {
-    if ($('html').attr('data-status-current') > 0) {
-      clearInterval(current);
-      $('html').attr('data-status-current', 0);
-    }
-    getRemainingTime(current);
-    remaining = setInterval("getRemainingTime()", 500);
+    $('html').attr('data-remaining-time-sec', '900');
+    getRemainingTime();
+    remaining = setInterval("getRemainingTime()", 1000);
     mode = 'current';
   }
   $('html').attr('data-mode', mode);
 }
 
 function getCurrentTime() {
-  $('html').attr('data-status-current', 1);
-  var date = new Date();
-  var hour = makeNumSameLength(date.getHours(), 2);
-  var min = makeNumSameLength(date.getMinutes(), 2);
-  var sec = makeNumSameLength(date.getSeconds(), 2);
+  if ($('html').attr('data-status-remaining') > 0) {
+    clearInterval(remaining);
+    $('html').attr('data-status-remaining', '0');
+  }
+  $('html').attr('data-status-current', '1');
+  date = new Date();
+  hour = makeNumSameLength(date.getHours(), 2);
+  min = makeNumSameLength(date.getMinutes(), 2);
+  sec = makeNumSameLength(date.getSeconds(), 2);
   $('#time').html(hour + ':' + min + ':' + sec);
 }
 
 function getRemainingTime() {
-  $('html').attr('data-status-remaining', 1);
-  $('#time').html('15:00:00');
+  if ($('html').attr('data-status-current') > 0) {
+    clearInterval(current);
+    $('html').attr('data-status-current', '0');
+  }
+  $('html').attr('data-status-remaining', '1');
+  date = $('html').attr('data-remaining-time-sec');
+  secRaw = date % 60;
+  sec = makeNumSameLength(secRaw, 2);
+  minRaw = (date - secRaw) / 60;
+  min = makeNumSameLength(minRaw, 2);
+  $('#time').html(min + ':' + sec);
+  date = date - 1;
+  $('html').attr('data-remaining-time-sec', date);
 }
 
-function makeNumSameLength (num, figures) {
+function makeNumSameLength(num, figures) {
   var num = String(num);
   while (num.length < figures) {
     num = '0' + num;
